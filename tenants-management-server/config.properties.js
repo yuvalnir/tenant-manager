f7099a8e443ea2f8f7eea08414322b7c92c3fc13
yuvalnir <yuvalnir11@gmail.com>
2021-02-11 23:01:52 +0200

    delete

--- tenants-management-server/config.properties.js
+++
@@ -1,7 +0,0 @@
-const connectionString = `mongodb+srv://yuval-admin:msbotxz1@cluster0.ayy1d.mongodb.net/TenantsManagement?retryWrites=true&w=majority`
-const secretKey = "qfFm0xwTcCO0Eq7xGp8wsAlJ2iLm84zrMulG7TSG7YRVLIexdrpC6JoF1nzNd0p"
-
-module.exports = {
-    connectionString,
-    secretKey,
-}
